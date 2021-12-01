41d227f577836f0a8254666a5e5d08efc1c0dcac
Pranathi Sri Vallabhaneni <pranathi.pranaya@gmail.com>
2021-12-01 10:35:14 +0530

    Updated main

--- src/main/java/com/javatpoint/SpringBootHelloWorldExampleApplication.java
+++ src/main/java/com/javatpoint/SpringBootHelloWorldExampleApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootHelloWorldExampleApplication
 {
-public static void main(String[] args) 
-{
-SpringApplication.run(SpringBootHelloWorldExampleApplication.class, args);
-}
+	public static void main(String[] args) 
+	{
+		System.out.println("Inside Main");
+		SpringApplication.run(SpringBootHelloWorldExampleApplication.class, args);
+	}
 }
